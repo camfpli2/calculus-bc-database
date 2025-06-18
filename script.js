d9c896aa02b05c8320ba18155316e0da2f4f80f5
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2025-06-18 12:12:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -228,7 +228,7 @@ function touchEnded() {
 
 function adjustImageDimensions(){
   for(var v=0;v<questions.length;v++){
-
+   console.log(questions[v].w, questions[v].h);
     if((questions[v].width>wdth-40)||(questions[v].height>hgt-130)){      //too tall and too wide
       if(questions[v].width/(wdth-40)>questions[v].height/(hgt-130)){    //%wise, wider than taller
          questions[v].resize(wdth-40,0);
