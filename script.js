36ff851d2cb0fe0f26565fd8e0cbb854f84e1d39
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2025-06-20 17:46:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ function yearHomeScreen(){
   background(240);
 }
 
+function wholeSkillHomeScreen(){
+  background(240);
+}
+
 
 function setup(){
 
@@ -438,6 +442,7 @@ class control{
   tapit(){
     if(mouseX>=this.x && mouseX<=this.x+this.w && mouseY>=this.y && mouseY <= this.y+this.h && this.there){
       if(this.ind<=8){  // user clicks a unit
+        if(this.rgb===selectcolor){wholeSkillHomeScreen(this.ind);}  //user has d-clicked and this will bypass to whole skill mix
         for(var k=0;k<9;k++){
           controls[k].rgb=butcolor;
         }
