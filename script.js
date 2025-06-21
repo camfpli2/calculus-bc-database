ba8b9238cff3036c269c705b3474a89d2521defc
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2025-06-20 22:06:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function loadQuestions(ask){   //ask could be "year" , integer, or skill(decimal
     else if(ask===2012){load2012();}
     else if(ask===2008){load2008();}
   }
-  console.log(questions[0].width);
+  console.log(questions);
   adjustImageDimensions();
 }
 
