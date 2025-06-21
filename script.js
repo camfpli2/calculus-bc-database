16c9d97e3ce821cbc6c29a015698abbe633c2b2a
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2025-06-20 21:16:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,15 +33,16 @@ function yearHomeScreen(y){
       controls[p].drawit();
     }
   }
-  textSize(40);
-  noStroke();
-  fill(textcolor);
-  textAlign(CENTER);
-  text("AP Calculus BC: "+y,wdth/2,250);
-  text("Multiple Choice Section",wdth/2,300);
-  textSize(26);
-  text("1 - 28, No Calculator",wdth/2,350);
-  text("76 - 92, Calculator",wdth/2,500);
+  refresher();
+  // textSize(40);
+  // noStroke();
+  // fill(textcolor);
+  // textAlign(CENTER);
+  // text("AP Calculus BC: "+y,wdth/2,250);
+  // text("Multiple Choice Section",wdth/2,300);
+  // textSize(26);
+  // text("1 - 28, No Calculator",wdth/2,350);
+  // text("76 - 92, Calculator",wdth/2,400);
 }
 
 function wholeSkillHomeScreen(i){
